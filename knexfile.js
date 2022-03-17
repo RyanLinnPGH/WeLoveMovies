e847a6f639a74317c7b87fc606b9f19ad493eca6
RyanLinnPGH <94587248+RyanLinnPGH@users.noreply.github.com>
2022-03-17 14:47:02 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://jwdqutjt:h0zXXCRgpdLTvOP2qUHzoJ7WQCl4sMWf@salt.db.elephantsql.com/jwdqutjt",
 } = process.env;
 
 module.exports = {
