c75b3e41c428b2d9b7e4e68db8e9b14947df5a39
RyanLinnPGH <94587248+RyanLinnPGH@users.noreply.github.com>
2022-03-24 02:02:18 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,15 +9,18 @@ const notFound = require("./errors/notFound");
 const moviesRouter = require("../src/movies/movies.router");
 const theatersRouter = require("../src/theaters/theaters.router");
 const reviewsRouter = require("../src/reviews/reviews.router");
+const res = require("express/lib/response");
 
 // middleWare
 app.use(cors());
 app.use(express.json());
 
 // pipeLine
+
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/", (req, res) => res.status(200).send("This is the homepage"))
 
 // error handling
 app.use(notFound);
